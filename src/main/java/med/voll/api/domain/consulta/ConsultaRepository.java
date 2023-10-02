a0c31a42b370e55e47952df464241195ce65fe6e
Matheus Silvério <matheussilveriodev@outlook.com>
2023-10-02 13:55:08 -0300

    atualizacao

---
+++ src/main/java/med/voll/api/domain/consulta/ConsultaRepository.java
@@ -0,0 +1,17 @@
+package med.voll.api.domain.consulta;
+
+import med.voll.api.domain.paciente.Paciente;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+
+public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
+        boolean existsByMedicoIdAndData(Long idMedico, LocalDateTime data);
+
+        boolean existsByPacienteIdAndDataBetween(Long idPaciente, LocalDateTime primeiroHorario, LocalDateTime ultimoHorario);
+    }
+
+
+
